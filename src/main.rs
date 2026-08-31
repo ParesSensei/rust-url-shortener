d8a044a3a1bfca2e35aee5cebcfd64d95358c3d6
Pares <alfariz2004@gmail.com>
2026-08-31 22:12:05 +0700

    feat: testing sending payload url

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router};
+use axum::routing::{get, post, Route};
+use serde::{Deserialize, Serialize};
+#[tokio::main]
+async fn main() {
+
+    let app = Router::new()
+        .route("/", get(|| async { "URL Shortener is running!" }))
+        .route("/post_url", post(post_url))
+        .route("/get_url", get(get_url));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("Listening on: {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct UrlPayload {
+    url: String,
+}
+
+// #[axum::debug_handler]
+async fn post_url(
+    Json(payload): Json<UrlPayload>,
+) -> Json<UrlPayload> {
+    // let paylod = UrlPayload{
+    //     url: "https://docs.rs/tokio/latest/tokio/".to_string()
+    // };
+    Json(payload)
+}
+
+async fn get_url() -> String {
+    "shortccode ...".to_string()
 }
