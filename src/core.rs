1fba6c584f176b3bd00dd74feea70353c0ef521b
Pares <alfariz2004@gmail.com>
2026-09-05 02:53:55 +0700

    feat: small refactor function from main to core

--- src/core.rs
+++ src/core.rs
@@ -1,7 +1,11 @@
 use std::collections::HashMap;
-use rand::prelude::IteratorRandom;
+use axum::extract::State;
+use axum::Json;
 use rand::thread_rng;
 use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
+use crate::{AppState};
+
 #[test]
 pub fn generate_short_code() {
     // let mut pair = HashMap::<String, String>::new();
@@ -16,9 +20,9 @@ pub fn generate_short_code() {
     println!("choices: {:?}", choices3.choose(&mut rng));
 }
 
-#[test]
-fn shortcode() {
-    let mut short_code = String::new();;
+
+fn shortcode() -> String {
+    let mut short_code = String::new();
     let choices: Vec<char> = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().collect();
     let mut rng = thread_rng();
     for _ in 0..8{
@@ -28,8 +32,31 @@ fn shortcode() {
     };
     println!("short_code = {}", &short_code);
 
-    let user_data = "https://docs.rs/tokio/latest/tokio/".to_string();
-    let mut data = HashMap::new();
-    data.insert(short_code, user_data);
-    println!("data = {:?}", &data);
+    // let user_data = "https://docs.rs/tokio/latest/tokio/".to_string();
+    // let mut data = HashMap::new();
+    // data.insert(short_code, user_data);
+    // println!("data = {:?}", &data);
+    short_code
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UrlPayload {
+    url: String,
+}
+
+pub async fn get_url() -> String {
+    "shortcode ...".to_string()
+}
+
+pub async fn post_url(
+    State(state): State<AppState>,
+    Json(payload): Json<UrlPayload>,
+) -> Json<UrlPayload> {
+    // let paylod = UrlPayload{
+    //     url: "https://docs.rs/tokio/latest/tokio/".to_string()
+    // };
+    let mut data = state.data.lock().await;
+    let mut rng = thread_rng();
+
+    Json(payload)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,34 +3,29 @@ mod core;
 use axum::{Json, Router};
 use axum::routing::{get, post};
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use crate::core::{post_url, get_url};
+
+#[derive(Clone)]
+struct AppState {
+    data : Arc<Mutex<HashMap<String, String>>>,
+}
+
 #[tokio::main]
 async fn main() {
 
+    let state = AppState {
+        data : Arc::new(Mutex::new(HashMap::new())),
+    };
     let app = Router::new()
         .route("/", get(|| async { "URL Shortener is running!" }))
         .route("/post_url", post(post_url))
-        .route("/get_url", get(get_url));
+        .route("/get_url", get(get_url))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
     println!("Listening on: {}", listener.local_addr().unwrap());
     axum::serve(listener, app).await.unwrap();
-}
-
-#[derive(Deserialize, Serialize, Debug)]
-struct UrlPayload {
-    url: String,
-}
-
-// #[axum::debug_handler]
-async fn post_url(
-    Json(payload): Json<UrlPayload>,
-) -> Json<UrlPayload> {
-    // let paylod = UrlPayload{
-    //     url: "https://docs.rs/tokio/latest/tokio/".to_string()
-    // };
-    Json(payload)
-}
-
-async fn get_url() -> String {
-    "shortccode ...".to_string()
 }
